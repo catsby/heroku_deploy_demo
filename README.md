da120a5f2167b95cff5b394fd62d2513ab4265d5
Clint Shryock <clint@ctshryock.com>
2014-07-17 09:55:29 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple Go app, trying out [Nitrous.io](https://www.nitrous.io) integration with Heroku
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welp")
+}
+
+func main() {
+	fmt.Println("Trying gorilla mux")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
+}
